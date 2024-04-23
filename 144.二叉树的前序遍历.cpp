302e3fdd86510bb69def496242c33dcb97501f7d
Zwhy <zwhy2025@gmail.com>
2024-04-23 11:52:57 +0800

    [#1] [二叉树] (前序遍历递归)

---
+++ 144.二叉树的前序遍历.cpp
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=cpp
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <climits>
+#include <cctype>
+#include <cstring>
+#include <cassert>
+#include <numeric>
+#include <memory>
+using namespace std;
+
+#ifndef LISTNODE_H
+#define LISTNODE_H
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int value) : val(value), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+#endif
+
+class Solution
+{
+public:
+    void travTree(TreeNode *node, vector<int>& res)
+    {
+        if (node == nullptr) return;
+        // 中 前 后 -> 先序遍历
+        res.push_back(node->val);
+        travTree(node->left, res);
+        travTree(node->right, res);
+    }
+
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        vector<int> ret;
+        travTree(root, ret);
+        return ret;
+    }
+};
+// @lc code=end
