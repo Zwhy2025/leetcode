52cb7ca770c74297cad1be42d76fbead98f7c4df
Zwhy <2867878134@qq.com>
2024-05-07 14:05:36 +0800

    [#1] [二叉树] [丑陋的ac]

---
+++ 222.完全二叉树的节点个数.cpp
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=cpp
+ *
+ * [222] 完全二叉树的节点个数
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <climits>
+#include <cctype>
+#include <cstring>
+#include <cassert>
+#include <numeric>
+#include <memory>
+using namespace std;
+#ifndef LISTNODE_H
+#define LISTNODE_H
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode():val(0),left(nullptr),right(nullptr) {}
+    TreeNode(int value):val(value),left(nullptr),right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+#endif // LISTNODE_H
+
+class Solution {
+public:
+    int ret = 0;
+    void travTree(TreeNode* node){
+        if(!node) return;
+        ret++;
+        if(node->left) travTree(node->left);
+        if(node->right) travTree(node->right);
+
+    }
+
+    int countNodes(TreeNode* root) {
+       travTree(root);
+       return ret;
+    }
+};
+// @lc code=end
+
