b58a58c7324825a0137fa26bcf775dc690ee905c
Zwhy <2867878134@qq.com>
2024-05-09 02:16:49 +0800

    [1] [二叉树] (递归法还是有一丢丢抽象)

---
+++ 404.左叶子之和.cpp
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=404 lang=cpp
+ *
+ * [404] 左叶子之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <climits>
+#include <cctype>
+#include <cstring>
+#include <cassert>
+#include <numeric>
+#include <memory>
+
+using namespace std;
+#ifndef LISTNODE_H
+#define LISTNODE_H
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int value) : val(value), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+#endif // LISTNODE_H
+
+class Solution
+{
+public:
+    int travTree(TreeNode *node)
+    {
+        // 判断叶子节点
+        // 判断左边叶子节点
+        if (!node)
+            return 0;
+
+        int left = travTree(node->left);
+        if (node->left && 
+                !node->left->left &&
+                !node->left->right){
+            
+            left = node->left->val;
+        }
+        int right = travTree(node->right);
+
+        return left + right;
+    }
+
+    int sumOfLeftLeaves(TreeNode *root)
+    {
+        return travTree(root);
+    }
+};
+// @lc code=end
