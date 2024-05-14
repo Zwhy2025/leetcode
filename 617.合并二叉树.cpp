d4bfcf520dba0b847337cedf8071deb022d36f86
Zwhy <2867878134@qq.com>
2024-05-14 14:04:02 +0800

    [1] [二叉树] (差异不美满)

---
+++ 617.合并二叉树.cpp
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=617 lang=cpp
+ *
+ * [617] 合并二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <climits>
+#include <cctype>
+#include <cstring>
+#include <cassert>
+#include <numeric>
+#include <memory>
+using namespace std;
+
+#ifndef LISTNODE_H
+#define LISTNODE_H
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int value) : val(value), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+#endif // LISTNODE_H
+
+class Solution
+{
+public:
+    TreeNode *mergeTrees(TreeNode *root1, TreeNode *root2)
+    {
+        if (!root1) {
+            return root2;
+        }
+        if (!root2){
+            return root1;
+        }
+        
+        TreeNode* node = new TreeNode();
+        node->val = root1->val + root2->val;
+        node->left = mergeTrees(root1->left, root2->left);
+        node->right = mergeTrees(root1->right, root2->right);
+        return node;
+    }
+};
+// @lc code=end
