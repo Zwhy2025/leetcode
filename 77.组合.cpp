d1a644bcdca8fe1e05700ed90ca970da31b89623
Zwhy <2867878134@qq.com>
2024-05-21 22:44:09 +0800

    [1] [回溯法] (递归解决多层循环)

---
+++ 77.组合.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=77 lang=cpp
+ *
+ * [77] 组合
+ */
+
+// @lc code=start
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <climits>
+#include <cctype>
+#include <cstring>
+#include <cassert>
+#include <numeric>
+#include <memory>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int> path;
+
+    void backtracking(int n,int k, int startIndex){
+        if(path.size()==k){
+            result.push_back(path);
+            return;
+        }
+
+        for(int i = startIndex; i<=n;i++){
+            path.push_back(i);
+            backtracking(n,k,i+1);
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        result.clear();
+        path.clear();
+        backtracking(n,k,1);
+        return result;
+    }
+};
+// @lc code=end
+
