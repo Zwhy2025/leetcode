7dfcd943418b55851346761bcaeeefbb391fb8c7
Zwhy <2867878134@qq.com>
2024-06-21 11:08:50 +0800

    [1] [动态规划] (状态方程)

--- 509.斐波那契数.cpp
+++ 509.斐波那契数.cpp
@@ -24,12 +24,24 @@
 #include <memory>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+    int fib(int n){
+
+        if (n <= 1) return n;
+
+        //状态空间初始化
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++){
+            //状态方程
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 };
 // @lc code=end
-
