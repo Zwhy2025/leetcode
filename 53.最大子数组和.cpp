176f62637db1dc18d1c3cb86bcdb7c90662af472
Zwhy <2867878134@qq.com>
2024-06-22 12:48:36 +0800

    [1] [数组] (动态规划)

--- 53.最大子数组和.cpp
+++ 53.最大子数组和.cpp
@@ -29,20 +29,21 @@ class Solution
 public:
     int maxSubArray(vector<int> &nums)
     {
-        // 数组需要检索的大小有 1-nums.size()
-        vector<int> sums;
-        for (int i = nums.size(); i > 0; i--){
-            //std::cout <<"i: "<< i << std::endl;
-            for (int j = 0; j < nums.size(); j++){
-                //std::cout <<"j: "<< j << std::endl;
-                //std::cout <<"sum: "<< sum << std::endl;
-                sums.emplace_back(std::accumulate(j+nums.begin(),j+nums.begin()+i,0));
-                if ((j + nums.begin() + i) == nums.end()){
-                    break;
-                }
-            }
+        if(nums.size()<2){
+            return nums.back();
         }
-        return *std::max_element(sums.begin(), sums.end());
+        vector<int> dp(nums.size());
+        dp[0] = nums[0];
+
+        int res = dp[0];
+        // dp[i] 的含义是,从原数组开头到以i结尾的子数组的最大值
+        for (int i = 1; i < nums.size(); i++)
+        {
+            dp[i]=std::max(nums[i]+dp[i-1],nums[i]);
+            res = dp[i] >res ? dp[i] : res;
+        }
+
+        return res;
     }
 };
 // @lc code=end
