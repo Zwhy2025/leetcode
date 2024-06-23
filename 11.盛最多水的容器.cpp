a8345e830ea94c6b5b75153c27408a0994ab8c3b
Zwhy <2867878134@qq.com>
2024-06-23 23:59:33 +0800

    [1] [双指针] (ez)

---
+++ 11.盛最多水的容器.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=cpp
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <climits>
+#include <cctype>
+#include <cstring>
+#include <cassert>
+#include <numeric>
+#include <memory>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int res = 0;
+        for (int i = 0, j = height.size() - 1; i < j;)
+        {
+            int h = std::min(height[i], height[j]);
+            int area = h * (j - i);
+
+            res = std::max(area, res);
+            if (height[i] > height[j]){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
