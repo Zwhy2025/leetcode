cb94978761e4bbf801404f216f90cb3f17986be6
Zwhy <2867878134@qq.com>
2024-06-29 12:12:03 +0800

    [#1] [链表] (快慢指针秒杀)

---
+++ 141.环形链表.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=cpp
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <climits>
+#include <cctype>
+#include <cstring>
+#include <cassert>
+#include <numeric>
+#include <memory>
+using namespace std;
+#ifndef LISTNODE_H
+#define LISTNODE_H
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+#endif // LISTNODE_H
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        auto fast = head;
+        while (fast && fast->next)
+        {
+            head = head->next;
+            fast = fast->next->next;
+            if (fast == head){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+// @lc code=end
