2a23755e48ef1726ebe7c5395ac06ef642f33764
Zwhy <2867878134@qq.com>
2024-07-04 11:27:27 +0800

    [1] [平衡二叉树] (其实不是很懂原理)

---
+++ 108.将有序数组转换为二叉搜索树.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=cpp
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+ // @lc code=start
+ /**
+  * Definition for a binary tree node.
+  * struct TreeNode {
+  *     int val;
+  *     TreeNode *left;
+  *     TreeNode *right;
+  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  * };
+  */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <climits>
+#include <cctype>
+#include <cstring>
+#include <cassert>
+#include <numeric>
+#include <memory>
+using namespace std;
+
+#ifndef LISTNODE_H
+#define LISTNODE_H
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() :val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int value) :val(value), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+#endif // LISTNODE_H
+class Solution {
+public:
+    TreeNode* tickTree(vector<int>& nums, int start, int end)
+    {
+        if (start > end) {
+            return nullptr;
+        }
+
+        int mid = start + (end - start) / 2;
+
+        auto root = new TreeNode(nums[mid]);
+
+        root->left = tickTree(nums, start, mid - 1);
+        root->right = tickTree(nums, mid + 1, end);
+        return root;
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums)
+    {
+        return tickTree(nums, 0, nums.size()-1);
+    }
+};
+// @lc code=end
+
