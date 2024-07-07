f427655fae9d4a88b9433c9789e2c713a8dfccca
Zwhy <2867878134@qq.com>
2024-07-07 13:25:32 +0800

    [#1] [二叉树] (暴力解)

---
+++ 114.二叉树展开为链表.cpp
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=cpp
+ *
+ * [114] 二叉树展开为链表
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <climits>
+#include <cctype>
+#include <cstring>
+#include <cassert>
+#include <numeric>
+#include <memory>
+using namespace std;
+
+#ifndef LISTNODE_H
+#define LISTNODE_H
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode():val(0),left(nullptr),right(nullptr) {}
+    TreeNode(int value):val(value),left(nullptr),right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+#endif // LISTNODE_H
+
+class Solution {
+public:
+    vector<TreeNode*> _vNodes;
+
+    void tickTree(TreeNode* node){
+        if(!node){
+            return;
+        }
+        _vNodes.push_back(node);
+        tickTree(node->left);
+        tickTree(node->right);
+    }
+    void flatten(TreeNode* root) {
+        tickTree(root);
+        TreeNode* dummy = new TreeNode(-1);
+        auto it = dummy;
+        for (int i = 0; i < _vNodes.size(); i++)
+        {
+            it->left =nullptr;
+            it->right =_vNodes[i];
+            it = it->right;
+        }
+        root = dummy;
+    }
+};
+// @lc code=end
+
