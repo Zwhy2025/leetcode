9aea241b495a0c5a936b120684d5b2b75e7f6da6
Zwhy <2867878134@qq.com>
2024-07-10 23:22:06 +0800

    [#1] [数组] (暴力解)

---
+++ 215.数组中的第k个最大元素.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=cpp
+ *
+ * [215] 数组中的第K个最大元素
+ */
+
+// @lc code=start
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+#include <climits>
+#include <cctype>
+#include <cstring>
+#include <cassert>
+#include <numeric>
+#include <memory>
+using namespace std;
+
+class Solution {
+public:
+
+
+    int findKthLargest(vector<int>& nums, int k) {
+
+       std::sort(nums.begin(),nums.end());
+       return nums[nums.size()-k];
+        
+    }
+};
+// @lc code=end
+
