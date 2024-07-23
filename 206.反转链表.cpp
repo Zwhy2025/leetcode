6f99665f12ef58e10288027be1ef62ba74803a2b
Zwhy <2867878134@qq.com>
2024-07-23 22:13:15 +0800

    [3] [链表] (头节点思维定势)

--- 206.反转链表.cpp
+++ 206.反转链表.cpp
@@ -4,66 +4,47 @@
  * [206] 反转链表
  */
 
-// @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <queue>
-#include <stack>
-#include <unordered_map>
-#include <map>
-#include <set>
-#include <unordered_set>
-#include <cmath>
-#include <climits>
-#include <cctype>
-#include <cstring>
-#include <cassert>
-#include <numeric>
-#include <memory>
-using namespace std;
+ // @lc code=start
+ /**
+  * Definition for singly-linked list.
+  * struct ListNode {
+  *     int val;
+  *     ListNode *next;
+  *     ListNode() : val(0), next(nullptr) {}
+  *     ListNode(int x) : val(x), next(nullptr) {}
+  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  * };
+  */
 
 #ifndef LISTNODE_H
 #define LISTNODE_H
 struct ListNode
 {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 #endif // LISTNODE_H
 
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* newNode =new ListNode();
-        auto cur = head;
-        while(head){
-            // 索引节点先走一步保存断点
-            head= head->next;
-
-            // 将目标节点指向新链表的虚拟头节点和第一个元素中间
-            cur->next = newNode->next;
-            newNode->next =cur;
-
-            //索引到下一次遍历
-            cur=head;
+    ListNode* reverseList(ListNode* head)
+    {
+        ListNode* left = nullptr;
+        ListNode* right = head;
+        ListNode* tmp = nullptr;
+        
+        // 有的时候是不需要头节点,做起来反而麻烦
+        while (right) {
+            tmp = right->next;
+            right->next = left;
+            left = right;
+            right = tmp;
         }
-        // 返回时去除虚拟头节点
-        return newNode->next;
+
+        return left;
     }
 };
 // @lc code=end
