06d740b2b96fb2560b8262ca49555c20ab489ad7
Zwhy <2867878134@qq.com>
2024-07-24 10:06:44 +0800

    [2] [字符串] (代码能力仍需加强)

--- 394.字符串解码.cpp
+++ 394.字符串解码.cpp
@@ -4,17 +4,18 @@
  * [394] 字符串解码
  */
 
-// @lc code=start
+ // @lc code=start
 #include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
 #include <queue>
 #include <stack>
 #include <unordered_map>
-#include <map>
-#include <set>
 #include <unordered_set>
+#include <algorithm>
 #include <cmath>
 #include <climits>
 #include <cctype>
@@ -26,35 +27,43 @@ using namespace std;
 
 class Solution {
 public:
-    string decodeString(string s) {
-        std::stack<std::pair<std::string, int>> stack;
-        std::string strCurrString;
-        int nCurrNum = 0;
-
-        for (auto &&ch : s)
-        {
-            if(std::isdigit(ch)){
-                nCurrNum =nCurrNum * 10+(ch - '0');
-            }else if (ch =='['){
-                stack.push({strCurrString,nCurrNum});
-                nCurrNum = 0;
-                strCurrString ="";
-            }else if(ch ==']'){
-                std::pair<std::string,int> top = stack.top();
-                stack.pop();
-                std::string strPrevString = top.first;
+    string decodeString(string s)
+    {
+        stack<std::pair<std::string, int>> st;
+        std::string currString;
+        int currNum = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (isdigit(s[i])) {       ///< 考虑高位数字
+                currNum = currNum * 10 + (s[i] - '0');
+            } else if (s[i] == '[') {  ///< 将历史子串转存,清空状态
+                st.push({ currString,currNum });
+                currNum = 0;
+                currString = "";
+            } else if (s[i] == ']') {  ///< 区间内的子串重复并累加
+                auto top = st.top();
+                st.pop();
+                string prevString = top.first;
                 int num = top.second;
-                for (int i = 0; i < num; i++)
-                {
-                    strPrevString += strCurrString;
+                for (int i = 0; i < num; i++) {
+                    prevString += currString;
                 }
-                strCurrString = strPrevString;
-            }else{
-                strCurrString += ch;
+                currString = prevString;
+            } else {                    ///< 生成区间内子串
+                currString += s[i];
             }
+
         }
-        return strCurrString;
+        return currString;
     }
 };
 // @lc code=end
 
+#include <gtest/gtest.h>
+TEST(Test394, SimpleTest)
+{
+    Solution s;
+    ASSERT_EQ("aaabcbc", s.decodeString("3[a]2[bc]"));
+    ASSERT_EQ("accaccacc", s.decodeString("3[a2[c]]"));
+    ASSERT_EQ("abcabccdcdcdef", s.decodeString("2[abc]3[cd]ef"));
+    ASSERT_EQ("abccdcdcdxyz", s.decodeString("abc3[cd]xyz"));
+}
