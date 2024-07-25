f781148d843dca652100670d2bafd7ec9e9e35e6
Zwhy <2867878134@qq.com>
2024-07-25 10:40:26 +0800

    [2] [字符串] (宝刀已老?)

--- 383.赎金信.cpp
+++ 383.赎金信.cpp
@@ -4,17 +4,18 @@
  * [383] 赎金信
  */
 
-// @lc code=start
+ // @lc code=start
 #include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
 #include <queue>
 #include <stack>
 #include <unordered_map>
-#include <map>
-#include <set>
 #include <unordered_set>
+#include <algorithm>
 #include <cmath>
 #include <climits>
 #include <cctype>
@@ -24,34 +25,39 @@
 #include <memory>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine)
     {
-
-        if (ransomNote.size() > magazine.size())
-        {
+        if (ransomNote.size() > magazine.size()) {
             return false;
         }
 
-        // 数值小的时候 数组做哈希表 效率优势大
+        // 构造杂志字典
         int alphabet[26];
-        for (auto it :magazine)
-        {
-            alphabet[it-'a']++;
+        for (auto it : magazine) {
+            alphabet[it - 'a']++;
         }
-        
-        for (auto it : ransomNote)
-        {
-           if(!alphabet[it-'a']){
-                return false ;
-           }else{
-               alphabet[it-'a']--;
-           }
+
+        // 遍历赎金信, 检查是否可以构造,如果缺少字母,则立即返回false
+        for (auto it : ransomNote) {
+            if (!alphabet[it - 'a']) {
+                return false;
+            } else {
+                alphabet[it - 'a']--;
+            }
         }
 
         return true;
     }
 };
 // @lc code=end
+
+#include <gtest/gtest.h>
+TEST(Test383, SimpleTest)
+{
+    Solution s;
+    EXPECT_EQ(s.canConstruct("cibdccdjhgihcdchibjfhhcafebfcdbjhfiebceeihfifbbecajfidfcfceghaddcjgcaajahbdcddebgafieehchgcigdggibjcajabbaghffgijjejcafajafagegfgihahjgajhhdfehgfgagedjbdcgdciiijciiceahbgbdfcifdadhgieiaadcjgifaaefcacgiaifiahidhchficddgaaaigdbbafabeciedcihbfefidfihcehfeihacbbjiffadbiiacjdidbfahehiicchecgjbbfdeaieafbdbhfigbihjiehjhjicbeaicbeeeefcgbihjajahjhfbddecijhhafdgbfbhicfdijecgchbcdccgajjbadfddjdbdaebehejdhjabecjgffafjbdcidhafchheefhjfciafiaffgbifgddeajfdeacgjfaaeahgaacefafcdadhaabajbdieahajfficfedhieaahdbhhbfgcicbbifbieeiheihgbebggfidfiggeaejgfgbbdjgchihibjghfceabcdjafabhceghegadgfcgeieigdjacbcecfgfhebidfajeifbgejfhhbajhidadgjfbcfgfdehcfgdbhhafgfbghfebfeccddbgebaeeghaccjiiiicgiibjfeacdbdfbahfefabgbaibbfgjcaccjjcggdidhajffacgdghcia", "b"), false);
+    EXPECT_EQ(s.canConstruct("aa", "ab"), false);
+    EXPECT_EQ(s.canConstruct("aa", "aab"), true);
+}
