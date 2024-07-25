e37b1a50bf786ad393768e75d15fbc93fc4a9c28
Zwhy <2867878134@qq.com>
2024-07-25 10:21:48 +0800

    [2] [数组] (其实是做出来了,可能是没有完全理解,还是前缀和)

--- 303.区域和检索-数组不可变.cpp
+++ 303.区域和检索-数组不可变.cpp
@@ -4,17 +4,18 @@
  * [303] 区域和检索 - 数组不可变
  */
 
-// @lc code=start
+ // @lc code=start
 #include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
 #include <queue>
 #include <stack>
 #include <unordered_map>
-#include <map>
-#include <set>
 #include <unordered_set>
+#include <algorithm>
 #include <cmath>
 #include <climits>
 #include <cctype>
@@ -26,19 +27,19 @@ using namespace std;
 
 class NumArray {
 public:
-    vector<int> _vSums;
-    NumArray(vector<int>& nums) {
-        _vSums.resize(nums.size()+1,0);
-
-        for (int i = 0; i < nums.size(); i++){
-            _vSums[i+1]=_vSums[i]+nums[i];
+    vector<int> _vPreSum;
+    NumArray(vector<int>& nums)
+    {
+        _vPreSum.resize(nums.size() + 1, 0);
+        for (int i = 1; i < _vPreSum.size(); i++) {
+            _vPreSum[i] = _vPreSum[i-1] + nums[i-1];
         }
-        
     }
-    
-    int sumRange(int left, int right) {
 
-        return _vSums[right+1]-_vSums[left];
+    int sumRange(int left, int right)
+    {
+        // 左闭右开
+        return _vPreSum[right+1]-_vPreSum[left] ;
     }
 };
 
@@ -47,5 +48,14 @@ public:
  * NumArray* obj = new NumArray(nums);
  * int param_1 = obj->sumRange(left,right);
  */
-// @lc code=end
+ // @lc code=end
 
+#include <gtest/gtest.h>
+TEST(Test303, SimpleTest)
+{
+    vector<int> nums = { -2, 0, 3, -5, 2, -1 };
+    NumArray s(nums);
+    EXPECT_EQ(s.sumRange(0, 2), 1);
+    EXPECT_EQ(s.sumRange(2, 5), -1);
+    EXPECT_EQ(s.sumRange(0, 5), -3);
+}
