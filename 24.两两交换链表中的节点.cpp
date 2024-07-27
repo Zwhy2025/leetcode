5dbfbcbe0e92bbe6c1a2063b79fc9e39bfe934cd
Zwhy <2867878134@qq.com>
2024-07-28 00:01:07 +0800

    [2] [链表] (这没做出来太离谱了)

--- 24.两两交换链表中的节点.cpp
+++ 24.两两交换链表中的节点.cpp
@@ -4,55 +4,52 @@
  * [24] 两两交换链表中的节点
  */
 
-// @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
+ // @lc code=start
+ /**
+  * Definition for singly-linked list.
+  * struct ListNode {
+  *     int val;
+  *     ListNode *next;
+  *     ListNode() : val(0), next(nullptr) {}
+  *     ListNode(int x) : val(x), next(nullptr) {}
+  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  * };
+  */
 #ifndef LISTNODE_H
 #define LISTNODE_H
 struct ListNode
 {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 #endif // LISTNODE_H
 
-class Solution
-{
+class Solution {
 public:
-    ListNode *swapPairs(ListNode *head)
+    ListNode* swapPairs(ListNode* head)
     {
-        auto faker = new ListNode();
-        faker->next = head;
-        auto pos = faker;
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        auto it = dummy;
 
-        while (pos->next != nullptr && pos->next->next!=nullptr)
-        {
-            //缓存
-            ListNode* back=pos->next;
-            ListNode* front=pos->next->next;
+        while (it->next && it->next->next) {    
+            auto back = it->next;
+            auto front = it->next->next;
 
-            // 交换
-            pos->next=front;
-            back->next =front->next;
-            front->next=back;
-           
-            //前进
-            pos=pos->next->next;
+            // 请记住 先实现 后优化
+            // 不然怎么称之为优化?
+            it->next = front;
+            back->next = front->next;
+            front->next =back;
+
+            it = it->next->next;
         }
-        //返回
-        return faker->next;
+
+        return dummy->next;
     }
 };
 // @lc code=end
+
