4847f585d31b9b61227735ff1a46d0c2caf3b04e
Zwhy <2867878134@qq.com>
2024-08-02 23:44:05 +0800

    [#2] [字符串] (ez)

--- 3110.字符串的分数.cpp
+++ 3110.字符串的分数.cpp
@@ -4,7 +4,7 @@
  * [3110] 字符串的分数
  */
 
-// @lc code=start
+ // @lc code=start
 #include <iostream>
 #include <vector>
 #include <string>
@@ -23,23 +23,17 @@
 #include <numeric>
 #include <memory>
 using namespace std;
-class Solution
-{
+
+class Solution {
 public:
     int scoreOfString(string s)
     {
-        int res = 0;
-        // 保证 s.size() - 2有意义
-        if (s.size() < 2)
-        {
-            return res;
+        int score = 0;
+        for (int i = 0; i < s.size() - 1; i++) {
+            score += std::abs(int(s[i + 1] - s[i]));
         }
-        // 遍历+强制类型转换
-        for (int i = 0; i <= s.size() - 2; i++)
-        {
-            res += std::abs(int(s[i] - s[i + 1]));
-        }
-        return res;
+        return score;
     }
 };
 // @lc code=end
+
