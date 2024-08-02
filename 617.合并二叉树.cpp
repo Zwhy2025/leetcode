ad75d06f8feeec684e6e9553317c1dbc39431630
Zwhy <2867878134@qq.com>
2024-08-02 23:30:44 +0800

    [2] [二叉树] (差两步美满,是还没理解递归本质吗?)

--- 617.合并二叉树.cpp
+++ 617.合并二叉树.cpp
@@ -4,19 +4,18 @@
  * [617] 合并二叉树
  */
 
-// @lc code=start
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
+ // @lc code=start
+ /**
+  * Definition for a binary tree node.
+  * struct TreeNode {
+  *     int val;
+  *     TreeNode *left;
+  *     TreeNode *right;
+  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  * };
+  */
 #include <iostream>
 #include <vector>
 #include <string>
@@ -35,37 +34,39 @@
 #include <numeric>
 #include <memory>
 using namespace std;
-
 #ifndef LISTNODE_H
 #define LISTNODE_H
+
 struct TreeNode
 {
     int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int value) : val(value), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() :val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int value) :val(value), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
 #endif // LISTNODE_H
 
-class Solution
-{
+class Solution {
 public:
-    TreeNode *mergeTrees(TreeNode *root1, TreeNode *root2)
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2)
     {
+        // 存在一侧儿树为nullptr,终止当前递归,返回另一侧哪怕也为nullptr
         if (!root1) {
             return root2;
         }
-        if (!root2){
+
+        if (!root2) {
             return root1;
         }
-        
-        TreeNode* node = new TreeNode();
-        node->val = root1->val + root2->val;
+
+        int val = root1->val + root2->val;
+        TreeNode* node = new TreeNode(val);
         node->left = mergeTrees(root1->left, root2->left);
         node->right = mergeTrees(root1->right, root2->right);
         return node;
     }
 };
 // @lc code=end
+
