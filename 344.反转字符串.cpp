afb8768ceab37d1fe1b8ff59238b086c7398b358
Zhao Jun <zwhy2025@gmail.com>
2024-08-21 23:43:02 +0800

    [#2] [数组] (chore)

--- 344.反转字符串.cpp
+++ 344.反转字符串.cpp
@@ -4,43 +4,29 @@
  * [344] 反转字符串
  */
 
-// @lc code=start
+ // @lc code=start
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
 #include <queue>
 #include <stack>
-#include <unordered_map>
-#include <map>
-#include <set>
-#include <unordered_set>
-#include <cmath>
-#include <climits>
-#include <cctype>
-#include <cstring>
-#include <cassert>
-#include <numeric>
-#include <memory>
+#include <deque>
+#include <algorithm>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    void swap(char& a, char& b)
-    {
-        auto temp = a;
-        a = b;
-        b = temp;
-    }
+    void reverseString(vector<char>& s) {
 
-    void reverseString(vector<char> &s)
-    {
-
-        for (int i = 0, j = s.size() - 1; i < j; i++, j--)
-        {
-            this->swap(s[i],s[j]);
+        for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
+            std::swap(s[i], s[j]);
         }
     }
+   
 };
-// @lc code=end
+ // @lc code=end
