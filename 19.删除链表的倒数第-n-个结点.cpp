fb447b43b55539c5e0be4fcd58ad2505a4caab0f
Zhao Jun <zwhy2025@gmail.com>
2024-08-22 23:59:05 +0800

    [#3] [链表] [百度一面]

--- 19.删除链表的倒数第-n-个结点.cpp
+++ 19.删除链表的倒数第-n-个结点.cpp
@@ -4,17 +4,17 @@
  * [19] 删除链表的倒数第 N 个结点
  */
 
-// @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+ // @lc code=start
+ /**
+  * Definition for singly-linked list.
+  * struct ListNode {
+  *     int val;
+  *     ListNode *next;
+  *     ListNode() : val(0), next(nullptr) {}
+  *     ListNode(int x) : val(x), next(nullptr) {}
+  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  * };
+  */
 #include <iostream>
 #include <vector>
 #include <string>
@@ -35,49 +35,44 @@
 using namespace std;
 #ifndef LISTNODE_H
 #define LISTNODE_H
-struct ListNode
-{
+struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 #endif // LISTNODE_H
 
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        
-        //虚拟头节点,更好处理当前节点
-        auto fake = new ListNode();
-        fake->next = head;
 
-        //构造快慢指针
-        auto front = fake;
-        auto back = fake;
+        auto dummy = new ListNode();
+        dummy->next = head;
 
-        // 快指针先走
-        while (n--){
-            front=front->next;
+        auto fast = dummy;
+        auto slow = dummy;
+
+        while (n--) {
+            if (!fast) {
+                return nullptr;
+            }
+            fast = fast->next;
         }
-        
-        while (front)
-        {
-            // 快指针先走一步,如果为空,
-            // 则back下一个就是我们需要处理的节点
-            front=front->next;
-            if(!front){
-                // 我们一定可以确定back的下一个节点不为空
-                // 因为题目中的n最小为1
-                back->next =back->next->next;
+
+        // 今天百度一面中需要手写数据结构以及自定义方法
+        // 一定要注意边界条件,否者会出现段错误,出错映像会很差
+        while (fast) {
+            fast = fast->next;
+            if (!fast) {
+                slow->next = slow->next->next;
                 break;
             }
-            // 无事发生则继续走
-            back=back->next;
+            slow = slow->next;
         }
-        //  记得返回的是虚拟头节点的下一个
-        return fake->next;
+
+        return dummy->next;
     }
 };
 // @lc code=end
