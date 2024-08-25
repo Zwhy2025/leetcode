707212511aa1540fb2d46f3662bf4e9b364e81cf
Zhao Jun <zwhy2025@gmail.com>
2024-08-25 22:51:57 +0800

    [#2] [二叉树] (递归方法)

--- 700.二叉搜索树中的搜索.cpp
+++ 700.二叉搜索树中的搜索.cpp
@@ -28,52 +28,24 @@ struct TreeNode {
     TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <list>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <unordered_set>
-#include <queue>
-#include <stack>
-#include <deque>
-#include <algorithm>
-using namespace std;
 #endif // LISTNODE_H
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-
         if (root == nullptr) {
             return nullptr;
         }
-        queue<TreeNode*> qe;
-        qe.push(root);
 
-        while (!qe.empty()) {
-            int size = qe.size();
-
-            for (int i = 0; i < size; i++) {
-                auto node = qe.front();
-                qe.pop();
-
-                if (node == nullptr) {
-                    continue;
-                }
-                else if (node->val < val) {
-                    qe.push(node->right);
-                }
-                else if (node->val > val) {
-                    qe.push(node->left);
-                }
-                else {
-                    return node;
-                }
-            }
+        if (root->val > val) {
+            return searchBST(root->left, val);
         }
-        return nullptr;
+        else if (root->val < val) {
+            return searchBST(root->right, val);
+        }
+        else {
+            return root;
+        }
+
     }
 };
 // @lc code=end
