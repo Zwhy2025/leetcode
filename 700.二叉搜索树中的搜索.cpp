a59d49eaf92c7d3d49f5fb6cbe0c520e9ac97e96
Zhao Jun <zwhy2025@gmail.com>
2024-08-25 22:51:05 +0800

    [#2] [二叉树] (非递归方法)

--- 700.二叉搜索树中的搜索.cpp
+++ 700.二叉搜索树中的搜索.cpp
@@ -4,66 +4,77 @@
  * [700] 二叉搜索树中的搜索
  */
 
-// @lc code=start
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <queue>
-#include <stack>
-#include <unordered_map>
-#include <map>
-#include <set>
-#include <unordered_set>
-#include <cmath>
-#include <climits>
-#include <cctype>
-#include <cstring>
-#include <cassert>
-#include <numeric>
-#include <memory>
-using namespace std;
+ // @lc code=start
+ /**
+  * Definition for a binary tree node.
+  * struct TreeNode {
+  *     int val;
+  *     TreeNode *left;
+  *     TreeNode *right;
+  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  * };
+  */
 
 #ifndef LISTNODE_H
 #define LISTNODE_H
-struct TreeNode
-{
+struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int value) : val(value), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() :val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int value) :val(value), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
-#endif // LISTNODE_H
 
-class Solution
-{
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <algorithm>
+using namespace std;
+#endif // LISTNODE_H
+class Solution {
 public:
-    TreeNode *searchBST(TreeNode *root, int val)
-    {
-        if (!root || root->val == val){
-            return root;
+    TreeNode* searchBST(TreeNode* root, int val) {
+
+        if (root == nullptr) {
+            return nullptr;
         }
-        TreeNode *node = nullptr;
-        if (root->val > val){
-            node = searchBST(root->left, val);
+        queue<TreeNode*> qe;
+        qe.push(root);
+
+        while (!qe.empty()) {
+            int size = qe.size();
+
+            for (int i = 0; i < size; i++) {
+                auto node = qe.front();
+                qe.pop();
+
+                if (node == nullptr) {
+                    continue;
+                }
+                else if (node->val < val) {
+                    qe.push(node->right);
+                }
+                else if (node->val > val) {
+                    qe.push(node->left);
+                }
+                else {
+                    return node;
+                }
+            }
         }
-        if (root->val < val){
-            node = searchBST(root->right, val);
-        }
-        return node;
+        return nullptr;
     }
 };
 // @lc code=end
+
