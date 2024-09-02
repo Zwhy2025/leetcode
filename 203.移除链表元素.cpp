f5695bb0c6667b495de05c9b46a085a065ba84b4
Zhao Jun <zwhy2025@gmail.com>
2024-09-02 23:46:27 +0800

    [#3] [链表] (面试笔试 拿捏)

--- 203.移除链表元素.cpp
+++ 203.移除链表元素.cpp
@@ -60,28 +60,24 @@ class Solution
 public:
     ListNode *removeElements(ListNode *head, int val)
     {
-        //增加虚拟头节点,统一处理真·头节点行为
-        auto faker =new ListNode(0);
-        faker->next = head;
-        // 增加移动指针,因返回结果时,需要返回原始链表头节点
-        auto pos = faker;
-        // 增加虚拟头之后,所有pos->next 则为遍历整个链表
-        while (pos->next != nullptr)
-        {
-            // 如果下一个节点为目标值,将下下个节点嫁接过来,
-            // 无论为空还是咋样,都被循环边界判定
-            if (pos->next->val == val)
-            {
-                pos->next = pos->next->next;
+
+        auto dummy = new ListNode(-1);
+        dummy->next = head;
+        auto it = dummy;
+
+        while (it->next) {
+
+            if (it->next->val == val) {
+                auto tmp = it->next;
+                it->next = it->next->next;
+                delete tmp;
             }
-            // 如果不为目标值,则移动指针向前
-            else
-            {
-                pos = pos->next;
+            else {
+                it = it->next;
             }
         }
-        //记得返回原始链表头
-        return faker->next;
+
+        return dummy->next;
     }
 };
 // @lc code=en              
