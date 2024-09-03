c269a23c1800b0d1c66dcfe752477626825b3e7e
Zhao Jun <zwhy2025@gmail.com>
2024-09-03 23:36:22 +0800

    [2] [数组] (...)

--- 287.寻找重复数.cpp
+++ 287.寻找重复数.cpp
@@ -24,14 +24,23 @@ using namespace std;
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int, bool> mapCaches;
-        for (auto&& num : nums) {
-            if (mapCaches[num]) {
-                return  num;
-            }
-            mapCaches[num] =true;
+        int nFast = nums[0];
+        int nSlow = nums[0];
+
+        // 第一步：将数组理解为一个链表找到相遇点
+        do {
+            nSlow = nums[nSlow];
+            nFast = nums[nums[nFast]];
+        } while (nSlow != nFast);
+
+        // 第二步：找到环的入口
+        nSlow = nums[0];
+        while (nSlow != nFast) {
+            nSlow = nums[nSlow];
+            nFast = nums[nFast];
         }
-        return INT_MAX;
+
+        return nSlow;
     }
 };
 // @lc code=end
