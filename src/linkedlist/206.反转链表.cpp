7e2203718e85e097c6833d8539ee98383f457198
Zhao Jun <zwhy2025@gmail.com>
2026-03-03 12:38:17 +0800

    feat: (1) 最优解

--- src/linkedlist/206.反转链表.cpp
+++ src/linkedlist/206.反转链表.cpp
@@ -4,48 +4,70 @@
  * [206] 反转链表
  */
 
- // @lc code=start
- /**
-  * Definition for singly-linked list.
-  * struct ListNode {
-  *     int val;
-  *     ListNode *next;
-  *     ListNode() : val(0), next(nullptr) {}
-  *     ListNode(int x) : val(x), next(nullptr) {}
-  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-  * };
-  */
+// @lc code=start
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {
+       // TODO
+   }
+ *     ListNode(int x) : val(x), next(nullptr) {
+       // TODO
+   }
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {
+       // TODO
+   }
+ * };
+ */
 
 #ifndef LISTNODE_H
 #define LISTNODE_H
 struct ListNode
 {
     int val;
-    ListNode* next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode* next) : val(x), next(next) {}
+    ListNode *next;
+    ListNode() : val(0), next(nullptr)
+    {
+        // TODO
+    }
+    ListNode(int x) : val(x), next(nullptr)
+    {
+        // TODO
+    }
+    ListNode(int x, ListNode *next) : val(x), next(next)
+    {
+        // TODO
+    }
 };
 #endif // LISTNODE_H
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* reverseList(ListNode* head)
+    ListNode *reverseList(ListNode *head)
     {
-        ListNode* left = nullptr;
-        ListNode* right = head;
-        ListNode* tmp = nullptr;
-        
-        // 有的时候是不需要头节点,做起来反而麻烦
-        while (right) {
-            tmp = right->next;
-            right->next = left;
-            left = right;
-            right = tmp;
+        // TODO
+
+        if (head == nullptr || head->next == nullptr)
+        {
+            return head;
         }
 
-        return left;
+        ListNode *last = nullptr;
+        ListNode *cur = head;
+
+        while (cur != nullptr)
+        {
+            ListNode *next = cur->next;
+            cur->next = last;
+            last = cur;
+            cur = next;
+        }
+        return last;
     }
 };
-// @lc code=end
 
+// @lc code=end
